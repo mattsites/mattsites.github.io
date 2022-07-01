40b605ef147f042d3f5001648b5c5255e77ccc89
mattsites <mattsites@outlook.com.au>
2022-07-01 10:29:19 +1000

    Removed the random colours
    It didn't work so I just add the colours in CSS instead.

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 const init = function(){
-	let items = document.querySelectorAll('section');
-	for (let i = 0; i < items.length; i++){
-		items[i].style.background = randomColor({luminosity: 'light'});
-	}
 	cssScrollSnapPolyfill()
 }
 init();
